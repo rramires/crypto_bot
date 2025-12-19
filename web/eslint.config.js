7b5731db8dc3eb048032b82fb9de02985e0154ba
rramires <ricardo@flexbr.com>
2025-12-19 13:55:27 -0300

    Add and conf Prettier and Import Sort.

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -3,27 +3,36 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
-  },
+	globalIgnores(['dist']),
+	{
+		files: ['**/*.{js,jsx}'],
+		plugins: {
+			'simple-import-sort': simpleImportSort,
+		},
+		extends: [
+			js.configs.recommended,
+			reactHooks.configs.flat.recommended,
+			reactRefresh.configs.vite,
+			eslintConfigPrettier,
+		],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+			parserOptions: {
+				ecmaVersion: 'latest',
+				ecmaFeatures: { jsx: true },
+				sourceType: 'module',
+			},
+		},
+		rules: {
+			'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]' }],
+			'prefer-const': 'warn',
+			'simple-import-sort/imports': 'error',
+			/* 'simple-import-sort/exports': 'error', */
+		},
+	},
 ])
