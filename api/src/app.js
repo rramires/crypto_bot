f971400d849f6daf37c660d39da2df11b45459ed
rramires <ricardo@flexbr.com>
2025-12-21 22:48:50 -0300

    Add Mids Helmet, Cors and Morgan.

--- api/src/app.js
+++ api/src/app.js
@@ -1,7 +1,21 @@
+import cors from 'cors'
 import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
 
 const app = express()
 
+// utils
+app.use(helmet())
+app.use(express.json())
+app.use(
+	cors({
+		origin: process.env.CORS_ORIGIN,
+	}),
+)
+app.use(morgan('dev'))
+
+// routes
 app.use('/', (req, res) => {
 	res.send('Hello from Express !')
 })
