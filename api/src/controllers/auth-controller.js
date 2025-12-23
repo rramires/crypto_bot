432f4464ace1b3062bea2d1d73ae35e12a624d8f
rramires <ricardo@flexbr.com>
2025-12-22 21:19:17 -0300

    Add JWT token.

--- api/src/controllers/auth-controller.js
+++ api/src/controllers/auth-controller.js
@@ -1,16 +1,34 @@
+import jwt from 'jsonwebtoken'
+
 export function doLogin(req, res) {
 	const { email, password } = req.body
 
 	if (email === 'fulano@email.com' && password === 'abc123') {
+		//
+		const token = jwt.sign({ id: 1 }, process.env.JWT_SECRET, {
+			expiresIn: parseInt(process.env.JWT_EXPIRES),
+		})
+
 		return res.json({
 			id: 1,
-			token: 'token',
+			token,
 		})
 	}
 
 	res.sendStatus(401)
 }
 
+const blacklist = {}
+
 export function doLogout(req, res) {
-	res.send('Logout')
+	const token = req.headers['authorization']
+
+	blacklist[token] = true
+	setTimeout(() => (blacklist[token] = undefined), parseInt(process.env.JWT_EXPIRES) * 1000)
+
+	res.sendStatus(200)
+}
+
+export function isBlackList(token) {
+	return blacklist[token]
 }
