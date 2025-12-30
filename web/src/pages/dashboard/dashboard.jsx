dc1ea9f6d82e96509e5b44f48b970a0ba848739d
rramires <ricardo@flexbr.com>
2025-12-29 22:21:21 -0300

    Add Dashboard basic structure.

--- web/src/pages/dashboard/dashboard.jsx
+++ web/src/pages/dashboard/dashboard.jsx
@@ -3,7 +3,19 @@ import { TemplatePage } from '../template-page'
 export function Dashboard() {
 	return (
 		<TemplatePage>
-			<h1>Dashboard</h1>
+			<div className='d-flex justify-content-between flex-wrap align-align-items-center py-4'>
+				<div className='d-block mb-4'>
+					<h1 className='h4'>Dashboard</h1>
+				</div>
+				<div className='btn-btn-toolbar mb-0'>
+					{/* TODO: Symbols select and new manual order button. */}
+				</div>
+				{/* TODO: Candle chart. */}
+				<div className='row'>
+					<div className='col-6'>{/* TODO: 24h market. */}</div>
+					<div className='col-6'>{/* TODO: Wallet. */}</div>
+				</div>
+			</div>
 		</TemplatePage>
 	)
 }
