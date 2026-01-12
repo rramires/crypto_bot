af4d04c5b009edf3480457a9137ef6147f48eff9
rramires <ricardo@flexbr.com>
2026-01-11 21:43:11 -0300

    Add Memory Tab with Search.

--- web/src/pages/brain/brain-tab.jsx
+++ web/src/pages/brain/brain-tab.jsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 
 export function BrainTab({ id, data, hasSearch = false }) {
 	const [search, setSearch] = useState('')
 
-	// FIXME: Remove after use this prop
-	id
+	const dataArr = useMemo(() => {
+		if (!data) {
+			return []
+		}
+
+		let arr = Object.entries(data).map((prop) => {
+			return {
+				key: prop[0],
+				value: prop[1],
+			}
+		})
+
+		if (search) {
+			arr = arr.filter((item) => item.key.indexOf(search) !== -1)
+		}
+
+		arr.sort((a, b) => {
+			if (a.key > b.key) return 1
+			if (a.key < b.key) return -1
+			return 0
+		})
+
+		return arr
+	}, [data, search])
 
 	function onSearchChange(event) {
 		setSearch(event.target.value.toUpperCase())
@@ -36,7 +58,18 @@ export function BrainTab({ id, data, hasSearch = false }) {
 							<th className='border-gray-200'>Value</th>
 						</tr>
 					</thead>
-					<tbody>{JSON.stringify(data)}</tbody>
+					<tbody>
+						{dataArr.map((obj) => (
+							<tr key={id + obj.key}>
+								<td>{obj.key}</td>
+								<td>
+									<pre>
+										{JSON.stringify(obj.value, null, 4)}
+									</pre>
+								</td>
+							</tr>
+						))}
+					</tbody>
 				</table>
 			</div>
 		</>
