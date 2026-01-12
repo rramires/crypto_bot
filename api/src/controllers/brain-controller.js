a496f2dc9dcecae5d101f3ccc305c42a637630b4
rramires <ricardo@flexbr.com>
2026-01-12 19:17:23 -0300

    Add updateBrainMemory.

--- api/src/controllers/brain-controller.js
+++ api/src/controllers/brain-controller.js
@@ -9,3 +9,20 @@ export async function getBrainMemory(req, res) {
 	const memory = await Brain.getInstance().getMemory()
 	res.json(memory)
 }
+
+export async function updateBrainMemory(req, res) {
+	const memoryKey = req.params.index // BTCUSDT:INDEX
+	const [symbol, idx] = memoryKey.split(':') // ['BTCUSDT', 'INDEX']
+
+	const index = idx.split('_')[0] // BTCUSDT:CANDLES_1h, BTCUSDT:RSI_1h
+	const interval = index.split('_')[1] // ex: 1h or undefined
+
+	const value = req.body.data
+
+	let results = await Brain.getInstance().updateMemory(symbol, index, interval, value, true)
+
+	if (results && results.length) {
+		results = results.filter(Boolean)
+	}
+	res.json(results)
+}
--- api/src/routers/brain-router.js
+++ api/src/routers/brain-router.js
@@ -1,7 +1,8 @@
 import express from 'express'
 export const brainRouter = express.Router()
 
-import { getBrain, getBrainMemory } from '../controllers/brain-controller.js'
+import { getBrain, getBrainMemory, updateBrainMemory } from '../controllers/brain-controller.js'
 
 brainRouter.get('/', getBrain)
 brainRouter.get('/memory', getBrainMemory)
+brainRouter.patch('/memory/:index', updateBrainMemory)
