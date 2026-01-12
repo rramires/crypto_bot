99d58c1fbcb4e44e772cebcbd1ff76d311421657
rramires <ricardo@flexbr.com>
2026-01-11 22:36:00 -0300

    Add Select in Tests Tab.

--- web/src/pages/brain/brain-test.jsx
+++ web/src/pages/brain/brain-test.jsx
@@ -1,3 +1,42 @@
+import { useState } from 'react'
+import Select from 'react-select'
+
 export function BrainTest({ data }) {
-	return <>{JSON.stringify(data)}</>
+	const [select, setSelect] = useState('')
+	const [index, setIndex] = useState('')
+	const [indexValue, setIndexValue] = useState({})
+
+	const customStyles = {
+		control: (provided) => ({
+			...provided,
+			width: 300,
+		}),
+	}
+
+	const options = Object.keys(data).map((item) => ({
+		label: item,
+		value: item,
+	}))
+
+	function onIndexChange(evt) {
+		setSelect({ label: evt.value, value: evt.value })
+		setIndex(evt.value)
+		setIndexValue(data[evt.value])
+	}
+
+	return (
+		<>
+			<div className='row'>
+				<div className='col-4'>
+					<Select
+						value={select}
+						styles={customStyles}
+						options={options}
+						onChange={onIndexChange}
+						onInputChange={(inputValue) => inputValue.toUpperCase()}
+					/>
+				</div>
+			</div>
+		</>
+	)
 }
